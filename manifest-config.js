e5f3edb32ad23617111b228be9b2cc3ed13bdd04
Yu Ling Cheng <cyuling23@gmail.com>
2022-12-07 13:50:30 +0100

    fix icon

--- manifest-config.js
+++ manifest-config.js
@@ -7,5 +7,5 @@ module.exports = {
   background_color: colors.background,
   theme_color: colors.primary,
   display: 'minimal-ui',
-  icon: 'media/DevUx_favicon.png',
+  icon: 'media/DevUx_favicon.ico',
 };
